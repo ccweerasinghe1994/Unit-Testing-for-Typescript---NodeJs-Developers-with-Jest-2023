c94ca724d3c705b498f7e0a0e2127d514d6c4542
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2023-10-20 10:34:51 +0530

    organising

--- src/app/Util.ts
+++ src/app/Util.ts
@@ -1,3 +1,20 @@
 export function toUpperCase(s: string): string {
 	return s.toUpperCase();
 }
+
+export type TStringInfo = {
+	loweCase: string;
+	upperCase: string;
+	length: number;
+	character: string[];
+	extraInfo?: object;
+};
+
+export function getStringInfo(s: string): TStringInfo {
+	return {
+		loweCase: s.toLowerCase(),
+		upperCase: s.toUpperCase(),
+		length: s.length,
+		character: s.split('')
+	};
+}
--- src/test/Util.test.ts
+++ src/test/Util.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from '../app/Util';
+import { toUpperCase, getStringInfo } from '../app/Util';
 
 describe('Utils test suite', () => {
 	it('should return upper case string of a valid string', () => {
@@ -10,5 +10,52 @@ describe('Utils test suite', () => {
 
 		// assert
 		expect(actual).toBe(expected);
+	});
+
+	describe('should return string info of a valid string', () => {
+		const sut = getStringInfo;
+
+		// arrange
+		const expected = {
+			loweCase: 'hello',
+			upperCase: 'HELLO',
+			length: 5,
+			character: ['h', 'e', 'l', 'l', 'o']
+		};
+		const actual = sut('hello');
+
+		describe('getStringInfo() for argument "hello"', () => {
+			it('should return the right lowercase string', () => {
+				expect(actual.loweCase).toBe(expected.loweCase);
+				expect(actual.loweCase).toHaveLength(expected.length);
+				expect(actual.loweCase).toContain<string>('h');
+			});
+
+			it('should return the right uppercase string', () => {
+				expect(actual.upperCase).toBe(expected.upperCase);
+				expect(actual.upperCase).toHaveLength(expected.length);
+				expect(actual.upperCase).toContain<string>('H');
+			});
+
+			it('should return the right length', () => {
+				expect(actual.length).toBe(expected.length);
+			});
+
+			it('should return the right character array', () => {
+				expect(actual.character).toEqual(expected.character);
+				expect(actual.character).toHaveLength(expected.length);
+				expect(actual.character).toContain<string>('h');
+				// when u don't know the order
+				expect(actual.character).toEqual(
+					expect.arrayContaining(['h', 'e', 'l', 'l', 'o'])
+				);
+			});
+
+			it('should return the right info', () => {
+				expect(actual.extraInfo).toBeUndefined();
+				expect(actual).not.toHaveProperty('extraInfo');
+				expect(actual.extraInfo).not.toBeTruthy();
+			});
+		});
 	});
 });
